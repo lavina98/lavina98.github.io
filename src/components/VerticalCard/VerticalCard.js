2795c2796b2a9588afc7f8d11724acfa8d5bbd95
Lavina Golani <luvina1009@gmail.com>
2020-06-12 16:42:02 +0530

    styling project page and vertical card component

--- src/components/VerticalCard/VerticalCard.js
+++ src/components/VerticalCard/VerticalCard.js
@@ -9,12 +9,12 @@ class VerticalCard extends React.Component {
   render() {
     return (
       <div  className= "verticalCard">  
-          <img src={this.props.image}></img>
+          <img src={this.props.image} className="vertical_card_image"></img>
           <h3>{this.props.heading}</h3>
           <h4>{this.props.subheading}</h4>
           <p>{this.props.content}</p>
-          <button>
-            <a href={this.props.link} target="blank">
+          <button className="vertical_card_button">
+            <a href={this.props.link} target="_blank">
                 {this.props.buttonContent}
             </a>
           </button>
