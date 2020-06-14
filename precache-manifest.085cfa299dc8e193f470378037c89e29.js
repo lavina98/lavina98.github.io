d1ca825d006c6384f7cc2d8f54830856edfbe4d2
Lavina Golani <luvina1009@gmail.com>
2020-06-15 04:02:49 +0530

    Updates

---
+++ precache-manifest.085cfa299dc8e193f470378037c89e29.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "513c82a99ee77a4ae9d251d7e3d7ec15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6a72bad338f719b6d69",
+    "url": "/static/css/main.a6572891.chunk.css"
+  },
+  {
+    "revision": "5f8fc19c0f1805696b49",
+    "url": "/static/js/2.85639e42.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.85639e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a72bad338f719b6d69",
+    "url": "/static/js/main.ad5e87b1.chunk.js"
+  },
+  {
+    "revision": "e02bb5bf66ae4ca55bc9",
+    "url": "/static/js/runtime-main.c56b8d88.js"
+  },
+  {
+    "revision": "cf88a9f28b096ed8c868a270c0ea25b3",
+    "url": "/static/media/achievements.cf88a9f2.png"
+  },
+  {
+    "revision": "9074c8fcd4a2eb27fcf3177816d2c0b1",
+    "url": "/static/media/backend.9074c8fc.png"
+  },
+  {
+    "revision": "82e15c5881a885307f56fbd0ec8a5141",
+    "url": "/static/media/biometric_authentication.82e15c58.png"
+  },
+  {
+    "revision": "b0dc44608459b99cac6a717c7536a27f",
+    "url": "/static/media/college_activity.b0dc4460.png"
+  },
+  {
+    "revision": "18e03f8e99cde2a9244deea06ed8fdfd",
+    "url": "/static/media/cybersecurity.18e03f8e.png"
+  },
+  {
+    "revision": "00167fa53b1c2ceddf1b62761f1a67cc",
+    "url": "/static/media/data_science.00167fa5.png"
+  },
+  {
+    "revision": "aca3caccc7ea0335d6f15d7741986702",
+    "url": "/static/media/diabetes_analysis.aca3cacc.png"
+  },
+  {
+    "revision": "53c213cdefb1d9f4c6bdb8b976f1610a",
+    "url": "/static/media/distributed_cache.53c213cd.png"
+  },
+  {
+    "revision": "9a9952d705bd68d644d3032cfd2f842b",
+    "url": "/static/media/education.9a9952d7.png"
+  },
+  {
+    "revision": "7a804cbfaa689f4a79b74f57fd8b224e",
+    "url": "/static/media/educational_portal.7a804cbf.png"
+  },
+  {
+    "revision": "ec1936a1c1cd8a647e353170f72e1f51",
+    "url": "/static/media/experience.ec1936a1.png"
+  },
+  {
+    "revision": "7a26777b628e1fddf31cdd411998ea95",
+    "url": "/static/media/frontend.7a26777b.png"
+  },
+  {
+    "revision": "3aaefcd1052d7acca9f4924a81ee24bd",
+    "url": "/static/media/lg.3aaefcd1.png"
+  },
+  {
+    "revision": "3ca0d846b745ed8033129294f2f8c189",
+    "url": "/static/media/logo.3ca0d846.png"
+  },
+  {
+    "revision": "ba0affa7d615f094d0de33763a8d38fd",
+    "url": "/static/media/machine_learning.ba0affa7.png"
+  },
+  {
+    "revision": "9ef909ce190b8e9218567a09c797747c",
+    "url": "/static/media/news_app.9ef909ce.png"
+  },
+  {
+    "revision": "67a076d13bdad1faa32af2991a0eb72b",
+    "url": "/static/media/portfolio_management.67a076d1.png"
+  },
+  {
+    "revision": "dc55a68aa4fa57fcaad9879c6e18cae7",
+    "url": "/static/media/projects.dc55a68a.png"
+  },
+  {
+    "revision": "4d78bc8a6b407dcfb89218b847c98653",
+    "url": "/static/media/spit.4d78bc8a.png"
+  },
+  {
+    "revision": "72c398c7ff949485f7ddf756693cf9bf",
+    "url": "/static/media/sudoku_solver.72c398c7.png"
+  },
+  {
+    "revision": "0fad4a673aa46ad5332a3b5bcd07f390",
+    "url": "/static/media/uxd.0fad4a67.png"
+  }
+]);
